a1f0179b143a9434a73baefd9c649083b51bdd48
CORP01\pkheni <pkheni@musicreports.com>
2023-12-07 16:54:21 +0530

    Modified index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+interface MainProps {
+  portletNamespace: string;
+  contextPath: string;
+  portletElementId: string;
+}
+
+const Main: React.FC<MainProps> = ({
+  portletNamespace,
+  contextPath,
+  portletElementId,
+}) => {
+  // Your component logic here
+
+  return <div>{/* Your JSX/React elements here */}</div>;
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Main
+      portletNamespace="yourPortletNamespace"
+      contextPath="yourContextPath"
+      portletElementId="yourPortletElementId"
+    />
   </React.StrictMode>
 );
 
