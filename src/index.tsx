ae05cadd1292c7b6ef5b07dfa198e32fc3f982bc
CORP01\pkheni <pkheni@musicreports.com>
2023-12-08 15:22:40 +0530

    Third commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,36 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-interface MainProps {
-  portletNamespace: string;
-  contextPath: string;
-  portletElementId: string;
-}
-
-const Main: React.FC<MainProps> = ({
-  portletNamespace,
-  contextPath,
-  portletElementId,
-}) => {
-  // Your component logic here
-
-  return <div>{/* Your JSX/React elements here */}</div>;
-};
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Main
-      portletNamespace="yourPortletNamespace"
-      contextPath="yourContextPath"
-      portletElementId="yourPortletElementId"
-    />
+    <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
