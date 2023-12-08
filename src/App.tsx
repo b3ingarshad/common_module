c4da507b0939f1450d17b4423742259ffdd89ec5
CORP01\pkheni <pkheni@musicreports.com>
2023-12-08 19:04:15 +0530

    14

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import React from "react";
 
 function App() {
-  return (
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
-  );
+  return <>Helo</>;
 }
 
 export default App;
